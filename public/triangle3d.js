33a476befb538f4ae0ccdffc00ab5811d6c75b83
Rolin Blake <rolinmblake@gmail.com>
2024-01-17 09:09:21 -0600

    refactoring and triangle3d.js addde

--- public/triangle3d.js
+++ public/triangle3d.js
@@ -11,11 +11,14 @@ function startWebgl() {
       return false;
     }
     console.log("WebGL has been successfully initialized");
-    const shaderVertexSrc = 'attribute vec2 position;\n\
+    const shaderVertexSrc = 'attribute vec3 position;\n\
+    uniform mat4 Pmatrix;\n\
+    uniform mat4 Vmatrix;\n\
+    uniform mat4 Mmatrix;\n\
     attribute vec3 color;\n\
     varying vec3 vColor;\n\
     void main(void){\n\
-    gl_Position = vec4(position, 0., 1.);\n\
+    gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);\n\
     vColor = color;}';
     const shaderFragmentSrc = 'precision mediump float;\n\
     varying vec3 vColor;\n\
@@ -40,6 +43,9 @@ function startWebgl() {
     GL.attachShader(shaderProgram, shaderVertex);
     GL.attachShader(shaderProgram, shaderFragment);
     GL.linkProgram(shaderProgram);
+    var _Pmatrix = GL.getUniformLocation(shaderProgram, "Pmatrix");
+    var _Vmatrix = GL.getUniformLocation(shaderProgram, "Vmatrix");
+    var _Mmatrix = GL.getUniformLocation(shaderProgram, "Mmatrix");
     var _color = GL.getAttribLocation(shaderProgram, "color");
     var _position = GL.getAttribLocation(shaderProgram, "position");
     GL.enableVertexAttribArray(_color);
@@ -47,9 +53,9 @@ function startWebgl() {
     GL.useProgram(shaderProgram);
     console.log("Successfully linked program shaders and is ready to render to context/target");
     var triVertices = [
-      -1, -1, 0, 0, 1, // vertex (-1,-1)
-      1, -1, 1, 1, 0,  // vertex (1,-1)
-      1, 1, 1, 0, 0   // vertex (1,1) 
+      -1, -1, 0, 0, 0, 1,  // vertex (-1, -1, 0)
+       1, -1, 0, 1, 1, 0,  // vertex ( 1, -1, 0)
+       1,  1, 0, 1, 0, 0   // vertex ( 1,  1, 0) 
     ];
     var triVbo = GL.createBuffer();
     GL.bindBuffer(GL.ARRAY_BUFFER, triVbo);
@@ -60,20 +66,36 @@ function startWebgl() {
     GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, triFbo);
     GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(triFaces), GL.STATIC_DRAW);
     console.log("Successfully bound triangle faces to WebGL rendering buffer");
+    var PROJMATRIX = UTILS.getProjection(40, CANVAS.width/CANVAS.height, 1, 100);
+    var VIEWMATRIX = UTILS.getId4();
+    var MOVEMATRIX = UTILS.getId4();
+    UTILS.translateZ(VIEWMATRIX, -5);
     GL.clearColor(0.0, 0.0, 0.0, 0.0);
+    GL.enable(GL.DEPTH_TEST);
+    GL.depthFunc(GL.LEQUAL);
+    GL.clearDepth(1.0);
+    var tlast = 0;
     console.log('Starting WebGL render to <canvas id="webgl-target"> context');
-    var animate = function() {
+    var animate = function(t) {
+      let dt = (t-tlast);
+      UTILS.rotateX(MOVEMATRIX, dt*0.0001);
+      UTILS.rotateY(MOVEMATRIX, dt*0.001);
+      UTILS.rotateZ(MOVEMATRIX, dt*0.0005);
+      tlast = t;
       GL.viewport(0, 0, CANVAS.width, CANVAS.height);
-      GL.clear(GL.COLOR_BUFFER_BIT);
+      GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
+      GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
+      GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
+      GL.uniformMatrix4fv(_Mmatrix, false, MOVEMATRIX);
       GL.bindBuffer(GL.ARRAY_BUFFER, triVbo);
-      GL.vertexAttribPointer(_position, 2, GL.FLOAT, false, 4*(2+3), 0);
-      GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 4*(2+3), 2*4);
+      GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 4*(3+3), 0);
+      GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 4*(3+3), 3*4);
       GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, triFbo);
       GL.drawElements(GL.TRIANGLES, 3, GL.UNSIGNED_SHORT, 0);
       GL.flush();
       window.requestAnimationFrame(animate);
     }
-    animate();
+    animate(0);
   }
   
   window.addEventListener("load", startWebgl);
---
+++ public/utils.js
@@ -0,0 +1,58 @@
+var UTILS = {
+  degToRad: function(theta) {return(theta*Math.PI/180);},
+  getProjection: function(theta, a, zMin, zMax) {
+    let tan = Math.tan(UTILS.degToRad(0.5*theta));
+    let A = -(zMax+zMin)/(zMax-zMin);
+    let b = (-2*zMax*zMin)/(zMax-zMin);
+    return [
+      0.5/tan, 0, 0, 0,
+      0, 0.5*a/tan, 0, 0,
+      0, 0, A, -1,
+      0, 0, b, 0
+    ];
+  },
+  getId4: function() {
+    return [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ];
+  },
+  rotateX: function(m, theta) {
+    let c = Math.cos(theta);
+    let s = Math.sin(theta);
+    let mv1 = m[1], mv5 = m[5], mv9 = m[9];
+    m[1] = m[1]*c-m[2]*s;
+    m[5] = m[5]*c-m[6]*s;
+    m[9] = m[9]*c-m[10]*s;
+    m[2] = m[2]*c+mv1*s;
+    m[6] = m[6]*c+mv5*s;
+    m[10] = m[10]*c+mv9*s;
+  },
+  rotateY: function(m, theta) {
+    let c = Math.cos(theta);
+    let s = Math.sin(theta);
+    let mv0=m[0], mv4=m[4], mv8=m[8];
+    m[0]=c*m[0]+s*m[2];
+    m[4]=c*m[4]+s*m[6];
+    m[8]=c*m[8]+s*m[10];
+    m[2]=c*m[2]-s*mv0;
+    m[6]=c*m[6]-s*mv4;
+    m[10]=c*m[10]-s*mv8;
+  },
+  rotateZ: function(m, theta) {
+    let c = Math.cos(theta);
+    let s = Math.sin(theta);
+    let mv0=m[0], mv4=m[4], mv8=m[8];
+    m[0]=c*m[0]-s*m[1];
+    m[4]=c*m[4]-s*m[5];
+    m[8]=c*m[8]-s*m[9];
+    m[1]=c*m[1]+s*mv0;
+    m[5]=c*m[5]+s*mv4;
+    m[9]=c*m[9]+s*mv8;
+  },
+  translateZ: function(m, x) {
+    m[14] += x;
+  }
+};
